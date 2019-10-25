79c761968eb5aba887d3a908e5c67a2f65f67657
Vina Kurniasari <sj9962nf@go.minneapolis.edu>
2019-10-24 20:45:52 -0500

    Connect the event api to the main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from weather import getWeather
 from restaurant_api import getRestaurants
-
+from event_api import getEvents
 def main():
 
     show_menu()
@@ -10,9 +10,10 @@ def show_menu():
               "Enter a city and country code to see"
               " the weather, top restaurants\n and concerts")
     print("You can find country codes at https://en.wikipedia.org/wiki/List_of_ISO_3166_country_codes ")
-    city, country_code = getInput()
+    city, country_code, keyword = getInput()
     getWeather(city, country_code)
     getRestaurants(city)
+    getEvents(city, keyword)
 
 
 
@@ -20,7 +21,8 @@ def show_menu():
 def getInput():
     city = input(("City: "))
     country_code = input(("Country Code: "))
-    return city, country_code
+    keyword = input(("Enter keyword for event: "))
+    return city, country_code, keyword
     # receiving user input
 
 
