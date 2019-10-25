f974c5db3c1deea3d0633d3cc7c8b1aab9380d7f
Vina Kurniasari <sj9962nf@go.minneapolis.edu>
2019-10-25 11:02:44 -0500

    Sort the event to display only 5 events

--- event_api.py
+++ event_api.py
@@ -21,12 +21,14 @@ def getEvents(location, keyword):
         
         # Fetch data from the eventful api
         url = 'http://api.eventful.com/json/events/search?'
-        params = {'keyword:' : keyword, 'location' : location, 'app_key' : event_key}
+        params = {'keyword:' : keyword, 'location' : location, 'page_size' : 5, 'app_key' : event_key}
         data = requests.get(url, params=params).json()
 
         events_data = data['events']['event']
 
         event_list = []
+
+        print(f'\n --EVENT INFORMATION IN {location}-- \n')
 
         for event in events_data:
             name = event['title']
@@ -36,7 +38,7 @@ def getEvents(location, keyword):
             event_data = EventData(name, place, address, date)
             event_list.append(event_data) # Add the needed data to the event_list
             print(event_data)
-            
+
         return event_list
 
     except Exception as e:
