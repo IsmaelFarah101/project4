3e8ffc87d2df9985704cb70eeb0eefc44f56d84a
tony <tony.spicer.covin@gmail.com>
2019-10-24 19:42:51 -0500

    added weather

---
+++ main.py
@@ -0,0 +1,38 @@
+from weather import getWeather
+
+
+def main():
+
+    show_menu()
+
+def show_menu():
+    print("****Travel App****\n"
+              "Enter a city and country code to see"
+              " the weather, top restaurants\n and concerts")
+    print("You can find country codes at https://en.wikipedia.org/wiki/List_of_ISO_3166_country_codes ")
+    city, country_code = getInput()
+    getWeather(city, country_code)
+
+
+
+
+
+def getInput():
+    city = input(("City: "))
+    country_code = input(("Country Code: "))
+    return city, country_code
+    # receiving user input
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+        main()
---
+++ weather.py
@@ -0,0 +1,44 @@
+import requests
+import os
+from datetime import datetime
+
+def getWeather(city, country_code):
+    query = {'q': city + "," + country_code, 'units': 'imperial', 'appid': '9db10307657b0ff8224b0da642ac57f7'}
+    url = 'http://api.openweathermap.org/data/2.5/forecast'
+    try:
+        data = requests.get(url, params=query).json()
+        forecast_items = data['list']
+        printcount = 0
+        values = [0,8,16,24,32]
+        daycount = 1
+        print("\n5 Day Forecast for " + city + "\n")
+        for forecast in forecast_items:
+            if printcount in values:
+                timestamp = forecast['dt']
+                weather_description = forecast['weather'][0]['description']
+                temp_f = forecast['main']['temp']
+                wind_speed = forecast['wind']['speed']
+                if (daycount) == 1:
+                    print("Today")
+                else:
+                    print('Day'+str(daycount))
+                print(f'Weather description: {weather_description}')
+                print(f'Temperature: {temp_f:.2f} F')
+                print(f'Wind speed: {str(wind_speed)} \n')
+                daycount += 1
+
+            printcount += 1
+
+        return True
+
+    except Exception as e:
+        print(e)
+        print("Please enter a valid city/country code\n")
+        return False
+
+
+
+
+
+
+
